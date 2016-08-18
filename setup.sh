49a3bd08e67130c1a09ee48770d6af7ad25ff9dd
Joe-noh <honzawa.j@pepabo.com>
2016-08-18 10:54:09 +0900

    link .spacemacs

--- setup.sh
+++ setup.sh
@@ -11,10 +11,5 @@ ln -s "$HERE/.gitconfig" ~/
 
 ln -s "$HERE/.vimrc" ~/
 
-if [ ! -f ~/.emacs.d ]; then
-  mkdir ~/.emacs.d
-fi
-
-ln -s "$HERE/.emacs.d/init.el"         ~/.emacs.d/
-ln -s "$HERE/.emacs.d/first_of_all.el" ~/.emacs.d/
+ln -s "$HERE/.spacemacs" ~/
 
