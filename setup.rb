35f035f2eab9ca550366c4feb41a889f75a89cec
Joe-noh <goflb.jh@gmail.com>
2017-01-12 12:43:22 +0900

    setupスクリプトはshellじゃなくてrubyで

---
+++ setup.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "optparse"
+require "fileutils"
+
+opts = ARGV.getopts("", "os:")
+
+os = opts["os"]
+
+raise "Give --os option" unless os
+raise unless Dir.exists?(os)
+
+entries = %w[
+  zshrc
+  tmux.conf
+  gitignore
+  gitconfig
+  vimrc
+  spacemacs
+]
+
+entries.each do |entry|
+  source = File.join(Dir.pwd, os, entry)
+  target = File.join(Dir.home, ".#{entry}")
+
+  puts "create symlink: #{source} -> #{target}"
+
+  FileUtils.ln_s(source, target, force: true)
+end
