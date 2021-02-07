6329e0ba3c753755cf2f607a91acd1550e3a341f
Joe-noh <goflb.jh@gmail.com>
2021-02-07 20:08:33 +0900

    chezmoi add

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+set GOPATH $HOME/Workspace
+set PATH $GOPATH/bin $PATH
+set PATH $HOME/.anyenv/bin $PATH
+set PATH /usr/local/opt/python@3.9/libexec/bin $PATH
+set ERL_AFLAGS "-kernel shell_history enabled"
+set KERL_BUILD_DOCS "yes"
+alias k=kubectl
+alias gfm='git pull'
+alias iem='iex -S mix'
+alias dc='docker-compose'
+source ~/.asdf/asdf.fish
